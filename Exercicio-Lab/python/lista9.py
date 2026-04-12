cb294336fd40a4d01922c58e6fdcd86c6caf5a8e
Fabricioosm <fabricioszczpaniak@gmail.com>
2026-04-12 17:52:58 -0300

    lsita 9 em casa

--- Exercicio-Lab/python/lista9.py
+++ Exercicio-Lab/python/lista9.py
@@ -13,7 +13,7 @@ print(maior)'''
 
 # Questão 2
 
-from ProxPrimo import primo
+'''from ProxPrimo import primo
 
 lista = [2, 5, 7, 9, 4, 6, 8, 11] 
 acu_primo = 0
@@ -24,4 +24,43 @@ while i < tam:
     if primo(lista[i]):
         acu_primo += 1
     i += 1
-print(acu_primo)
+print(acu_primo)'''
+
+# Questão 3
+
+'''lista = ['fabricio', 20, 7, 'medeiros', 5.5]
+ind = len(lista) -1
+
+while ind >= 0:
+    inversa = lista[ind]
+    print(inversa)
+    ind -= 1'''
+
+# Questão 4
+
+'''def soma_Lista_Par (lista):
+    ind = 0
+    tam = len(lista)
+    soma = 0
+
+    while ind < tam:
+        if lista[ind] %2 == 0:
+            soma = soma + lista[ind]
+        ind += 1
+    return soma
+print(soma_Lista_Par([1, 2, 3, 4, 5, 6, 8]))'''
+
+# Questão 5
+
+'''def num_lista (lista, num):
+    indice = 0
+    while indice < len(lista):
+        if num == lista[indice]:
+           return True
+        indice += 1
+    return False
+
+print(num_lista([1,2,3], 4))'''
+
+
+    
